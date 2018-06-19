400832c28c0d803c6f9901d571937e3bdf259b93
JonnyyJ <35937882+JonnyyJ@users.noreply.github.com>
2018-06-18 21:18:34 -0400

    Add files via upload

---
+++ test.php
@@ -0,0 +1,42 @@
+<?php
+
+
+class test
+{   
+
+   //新建验证
+    public function get1(Request $request=null)
+    {
+       
+
+        $data = [
+            'data'=>$authID,
+            'status' => (bool) $new,
+            'message' => $new ?'1' : '0',
+        ];
+
+        header('Content-type: application/json');
+        echo json_encode( $data );
+    }
+    //获取图像
+    public function get2(&$authID,&$type)
+  {
+    if(isset($authID['pic']))
+    {
+        
+        $pic = (get_magic_quotes_gpc()) ? stripslashes($authID['pic']) : $authID['pic'];
+    
+        //把下面这个路径变成你存在服务器上图片的路径
+        $pic = '/your/path/to/real/image/location/'.$pic;
+    
+       //获取图片的大小
+       $type = getimagesize($pic);
+    
+        //把获取的图片放入数据头中
+        header('Content-type: '.$type['mime']);
+    
+        //输出图片
+        readfile($pic);
+    }
+  }
+}
